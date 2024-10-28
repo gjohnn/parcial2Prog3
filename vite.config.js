207d86591f7b58acd3b202984e9c7d4d0c6857b7
gjohnn <juansgb316@outlook.com>
2024-10-28 18:18:29 +0000

    primer commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0', // Permite conexiones desde cualquier origen
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            clientPort: 5173 // Asegura que el puerto de HMR esté configurado correctamente
+        }
+    }
+});
